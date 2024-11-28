abbccfd154219c10f9acd952f56aed9fee67f745
Bas <55401336+BasToTheMax@users.noreply.github.com>
2024-11-28 19:06:07 +0000

    Add websocket server

--- src/index.js
+++ src/index.js
@@ -7,9 +7,29 @@ const log = new Logger(__filename);
 const express = require('express');
 const app = express();
 
+require('express-ws')(app);
+
 app.use((req, res, next) => {
     log.debug(`${req.method} ${req.url}`);
     next();
+});
+
+app.get('/ping', (req, res) => {
+    res.json({
+        ok: true,
+        version: '1.0.0'
+    })
+});
+
+app.ws('/game', (ws, req) => {
+    log.debug(`[WS] Connect`);
+    ws.on('message', (data) => {
+        ws.send("hello "+String(data)+"!");
+    });
+    ws.on('close', () => {
+        log.debug(`[WS] Disconnect`);
+    });
+    ws.send("hello!");
 });
 
 app.listen(env.PORT, () => {
